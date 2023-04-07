4d89168e4d8cf2ffa14682020ceeae407d0d2d95
ismailkhan2118 <ismailkhan2118@gmail.com>
2023-04-07 20:18:49 +0530

    Added Bfs Dfs Graph Traversal Visualizer

---
+++ src/components/Bfs.tsx
@@ -0,0 +1,244 @@
+import { on } from 'events'
+import React, { useMemo, useRef, useState } from 'react'
+import "./Bfs.scss"
+const Bfs = () => {
+
+    let delrow = [0, 1, -1, 0];
+    let delcol = [1, 0, 0, -1];
+
+    const numRows = useMemo(() => 20, [])
+    const numCols = useMemo(() => 30, [])
+
+    const initialGrid =
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+    const initialVisited =
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+    const initialVisitedColors =
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+
+    const [grid, setGrid] = useState(initialGrid);
+    const visited = useRef(initialVisited);
+    const visitedColors = useRef(initialVisitedColors);
+
+    const [source, setSource] = useState([-1, -1]);
+    const [dest, setDest] = useState([-1, -1]);
+    const lockSelection = useRef<number>(0);
+    const ongoing = useRef<boolean>(false);
+
+    const onSelect = (i: number, j: number) => {
+        if (!ongoing.current) {
+            if (lockSelection.current >= 2) {
+                setSource([i, j]);
+                setDest([-1, -1]);
+                lockSelection.current = 1;
+                return;
+            }
+            if (lockSelection.current === 0) {
+                setSource([i, j]);
+            } else {
+                setDest([i, j]);
+            }
+
+            lockSelection.current += 1;
+        }
+    }
+
+    const onStartBfs = () => {
+        if (!ongoing.current) {
+
+            if (source[0] === -1 || source[1] === -1 || dest[0] === -1 || dest[1] === -1) {
+                alert("start end points not selected!");
+                return;
+            }
+
+            ongoing.current = true;
+
+            let queue: number[][] = [];
+
+            queue.unshift([source[0], source[1], 0]);
+
+            let max_levels = 0;
+
+            while (queue.length !== 0) {
+                let vertex = queue.pop();
+                let level = vertex![2];
+                max_levels = level;
+
+                if (vertex && vertex[0] === dest[0] && vertex[1] === dest[1]) {
+                    console.log("REACHED");
+                    break;
+                }
+
+                for (let i = 0; i < delcol.length; i++) {
+                    let new_row = delrow[i] + vertex![0];
+                    let new_col = delcol[i] + vertex![1];
+
+                    if (new_row < numRows && new_row >= 0 && new_col < numCols && new_col >= 0 && visited.current[new_row][new_col] !== 1) {
+                        queue.unshift([new_row, new_col, level + 1]);
+                        visited.current[new_row][new_col] = 1;
+                        // eslint-disable-next-line no-loop-func
+                        setTimeout(() => {
+                            visitedColors.current[new_row][new_col] = 1;
+                            setSource([source[0], source[1]]);
+                            if (level === max_levels) {
+                                ongoing.current = false;
+                            }
+                        }, level * 100);
+                    }
+                }
+            }
+        }
+    }
+
+    const onStartDfs = () => {
+        if (!ongoing.current) {
+
+            if (source[0] === -1 || source[1] === -1 || dest[0] === -1 || dest[1] === -1) {
+                alert("start end points not selected!");
+                return;
+            }
+            ongoing.current = true;
+            let max_levels = 0;
+
+            const dfs = (vertex: number[], elapsed: number = 1) => {
+                visited.current[vertex[0]][vertex[1]] = 1;
+                max_levels = elapsed;
+
+                if (vertex && vertex[0] === dest[0] && vertex[1] === dest[1]) {
+                    setTimeout(() => {
+                        visitedColors.current[vertex[0]][vertex[1]] = 1;
+                        setSource([source[0], source[1]]);
+                        ongoing.current = false;
+                    }, elapsed * 100);
+                    console.log("REACHED");
+                    return 1;
+                }
+
+                setTimeout(() => {
+                    visitedColors.current[vertex[0]][vertex[1]] = 1;
+                    setSource([source[0], source[1]]);
+                }, elapsed * 100);
+
+                for (let i = 0; i < delcol.length; i++) {
+                    let new_row = delrow[i] + vertex![0];
+                    let new_col = delcol[i] + vertex![1];
+                    if (new_row < numRows && new_row >= 0 && new_col < numCols && new_col >= 0 && visited.current[new_row][new_col] !== 1) {
+                        if (dfs([new_row, new_col], elapsed + 1) === 1) {
+                            return 1;
+                        }
+                    }
+                }
+                return 0;
+            }
+            dfs([source[0], source[1]], 0);
+        }
+    }
+
+
+    const onReset = () => {
+        if (!ongoing.current) {
+            visited.current = initialVisited;
+            visitedColors.current = initialVisitedColors;
+            setGrid(initialGrid);
+            setSource([-1, -1])
+            setDest([-1, -1])
+        }
+    }
+
+    return (
+        <div className='bfs-grid'>
+            <h3>Famous Searches (Source to Destination)</h3>
+            <img src="/assets/algo-bg.jpg" alt="" />
+            <div>
+                {
+                    grid.map((row, i) => (
+                        <div key={i} className='row-container'>
+                            <div className='row'>
+                                {
+                                    row.map((cell, j) => (
+                                        <div key={j} onClick={onSelect.bind(null, i, j)} className={`row__cell ${visitedColors.current[i][j] === 1 && dest[0] === i && dest[1] === j ? 'row__cell--reached' : ''} ${visitedColors.current[i][j] === 1 ? 'row__cell--visited' : ''} ${source[0] === i && source[1] === j ? 'row__cell--source' : ''} ${dest[0] === i && dest[1] === j ? 'row__cell--destination' : ''}`}>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <div className='actions'>
+                <button className='start-btn' onClick={onStartDfs}>START LINEAR DIRECTION 1-D</button>
+                <button className='start-btn' onClick={onStartBfs}>START PARALLEL DIRECTIONS 4-D</button>
+                <button className='reset-btn' onClick={onReset}>RESET</button>
+            </div>
+        </div>
+    )
+}
+
+export default Bfs
